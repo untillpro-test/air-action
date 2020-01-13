8ba817f845860a785823607cf6f082c4b7538fbe
kvn <v.kudryavtsev@dev.untill.com>
2020-01-13 15:36:46 +0300

    self test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,19 @@
 
 echo "Hello $1!!!"
 
-echo `ls`
+echo "### ls -la"
+ls -la
+
+echo "### ls -ld"
+ls -ld
+
+echo "### sh --version"
+sh --version
+
+echo "### file -h /bin/sh"
+file -h /bin/sh
+
+echo "### find -L /bin -samefile /bin/sh"
+find -L /bin -samefile /bin/sh
 
 echo `git`
